5858816ce8cd6f4d79bafac74b34b0f899e19469
kurkova <kurkiewiczpaula@gmail.com>
2019-01-30 19:30:02 +0000

    First attempt to Heroku

---
+++ showtasks.bat
@@ -0,0 +1,21 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto browser
+echo.
+echo Problem with running script.
+goto fail
+
+:browser
+opera "http://localhost:8080/crud/v1/task/getTasks"
+timeout 30
+if "%ERRORLEVEL%" == "0" goto end
+echo.
+echo Problem with running opera browser.
+goto fail
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Work is finished
