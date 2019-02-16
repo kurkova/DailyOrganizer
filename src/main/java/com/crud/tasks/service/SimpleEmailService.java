fc2602f25c6cc487c4540d189612e2d3e319ee4a
kurkova <kurkiewiczpaula@gmail.com>
2019-02-16 22:34:45 +0000

    Task 19.3

---
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -0,0 +1,45 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class SimpleEmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleMailMessage.class);
+
+    public void send(final Mail mail) {
+        LOGGER.info("Starting email preparation");
+        try {
+            SimpleMailMessage mailMessage = createMailMessage(mail);
+            javaMailSender.send(mailMessage);
+            LOGGER.info("Email has been sent.");
+        } catch (MailException e) {
+            LOGGER.info("Failed to process email sending:", e.getMessage(), e);
+        }
+    }
+
+    private SimpleMailMessage createMailMessage(final Mail mail){
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        if(mail.getToCC() != null ){
+            mailMessage.setCc(mail.getToCC());
+            LOGGER.info("Carbon Copy added");
+        }
+        return mailMessage;
+    }
+}
+
