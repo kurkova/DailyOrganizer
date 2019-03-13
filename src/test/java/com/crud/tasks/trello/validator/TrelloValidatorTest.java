0152bcfa35d4e44dd79174465a5786c5e50716d4
kurkova <kurkiewiczpaula@gmail.com>
2019-03-13 18:13:38 +0000

    Corrected Task 21.2

--- src/test/java/com/crud/tasks/trello/validator/TrelloValidatorTest.java
+++ src/test/java/com/crud/tasks/trello/validator/TrelloValidatorTest.java
@@ -1,23 +1,15 @@
 package com.crud.tasks.trello.validator;
 
 import com.crud.tasks.domain.TrelloBoard;
-import com.crud.tasks.domain.TrelloList;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class TrelloValidatorTest {
-    @Autowired
-    TrelloValidator trelloValidator;
+    private TrelloValidator trelloValidator = new TrelloValidator();
 
     @Test
     public void testValidateTrelloBoards() {
