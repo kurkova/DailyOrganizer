6885f04076b46bad7f74f476a519d286977013d7
kurkova <kurkiewiczpaula@gmail.com>
2019-04-06 08:41:23 +0100

    Refactoring

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,13 +1,11 @@
 package com.crud.tasks.controller;
 
-
 import com.crud.tasks.domain.CreatedTrelloCardDto;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.facade.TrelloFacade;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @CrossOrigin(origins = "*")
@@ -30,14 +28,7 @@ public class TrelloController {
 }
 
 
-//    @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
-//    public List<TrelloBoardDto> getTrelloBoards() {
-//       List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-//        return trelloBoards.stream()
-//                .filter(s -> s.getId() != null)
-//                .filter(n -> n.getName().contains("Kodilla"))
-//               .collect(Collectors.toList());
-//    }
+
 
 
 
